f3197196e291cc8054f5a64f363664c9cfa41b71
Josh Hudnall <jhudnall@me.com>
2012-11-25 10:53:55 -0700

    Updated to be more robust
    The way photosets are wrapped seems to have changed or may be
    blog-dependent. This update removes reliance on a specific
    Tumblr-generated structure.

--- photosets.js
+++ photosets.js
@@ -44,7 +44,11 @@ function adjustIFrames() {
 }
 
 function adjustIFrame(iframe) {
+<<<<<<< HEAD
   var layout = $.parseJSON($(iframe).parent().attr('rel'));
+=======
+  var layout = $.parseJSON($(iframe).closest('.photoset-wrapper').attr('rel'));
+>>>>>>> Updated to be more robust
   var gutter = 10;
   
   $(iframe).contents().find('.photoset_row').width("100%");
